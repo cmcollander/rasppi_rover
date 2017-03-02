ef771b69567922cd5a34e2f3688674da77fb522c
Chris Collander <cmcollander@gmail.com>
2017-03-02 15:18:55 -0600

    Fixed more syntax and style issues

--- rover.py
+++ rover.py
@@ -3,38 +3,38 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 class Rover:
 	
 	def __init__(self,address=0x60):
-		self.mh = Adafruit_MotorHat(addr=address)
-		motors = []
+		self.mh = Adafruit_MotorHAT(addr=address)
+		self.motors = []
 		for i in range(1,5):
-			motors.append(mh.getMotor(i))
-			motors[i-1].setSpeed(0)
+			self.motors.append(self.mh.getMotor(i))
+			self.motors[i-1].setSpeed(0)
 	
 	def stop(self):
-		mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-		mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-		mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-		mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+		self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+		self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+		self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+		self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 	
 	def setWheel(self, mot, val):
 		if val<0:
-			motors[mot].run(Adafruit_motorHAT.BACKWARD)
-			motors[mot].setSpeed(-1*val)
+			self.motors[mot].run(Adafruit_MotorHAT.BACKWARD)
+			self.motors[mot].setSpeed(-1*val)
 		else:
-			motors[mot].run(Adafruit_motorHAT.FORWARD)
-			motors[mot].setSpeed(val)
+			self.motors[mot].run(Adafruit_MotorHAT.FORWARD)
+			self.motors[mot].setSpeed(val)
 	
 	def releaseWheel(self, mot):
-		motors[mot].run(Adafruit_MotorHAT.RELEASE)
+		self.motors[mot].run(Adafruit_MotorHAT.RELEASE)
 	
 	def move(self,left, right):
 		if -50<left<50:
 			left = 0
 		if -50<right<50:
 			right = 0
-		setWheel(0, left)
-		setWheel(1, left)
-		setWheel(2, right)
-		setWheel(3, right)
+		self.setWheel(0, left)
+		self.setWheel(1, left)
+		self.setWheel(2, right)
+		self.setWheel(3, right)
 		
 	def Joy2Mot(self, nJoyX, nJoyY, moveAlso=True):
 		fpivYLimit = 32.0
@@ -53,6 +53,6 @@ class Rover:
 		left_wheel = int(nMotMixL*1.9921875)
 		right_wheel = int(nMotMixR*1.9921875)
 		if moveAlso:
-			move(left_wheel,right_wheel)
+			self.move(left_wheel,right_wheel)
 		return [left_wheel,right_wheel]
 		
